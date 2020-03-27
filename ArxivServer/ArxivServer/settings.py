f1558fb8f5591eb3ea2936f08b8c3313061b8fde
hiyoung <hiyoungliu@gmail.com>
2020-03-27 11:59:10 +0800

    修改服务器ip地址

--- ArxivServer/ArxivServer/settings.py
+++ ArxivServer/ArxivServer/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
-        'NAME': 'arxiv',  # 数据库名，先前创建的
+        'NAME': 'YoungArxiv',  # 数据库名，先前创建的
         'USER': 'root',  # 用户名，可以自己创建用户
         'PASSWORD': 'liuhaiyang210',  # 密码
         'HOST': 'localhost',  # mysql服务所在的主机ip
--- ArxivServer/run.sh
+++ ArxivServer/run.sh
@@ -1,3 +1,3 @@
-python manage.py runserver 192.168.199.130:8000
+python manage.py runserver 192.168.199.215:8000
 # python manage.py makemigrations
 #python manage.py migrate
